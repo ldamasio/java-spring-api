c8a5ce770e4b7b4db1a2a22c7065519ba706270a
Leandro Damasio <ldamasio@gmail.com>
2024-05-18 07:26:40 -0300

    feat: initialize Spring Boot project with essential dependencies

---
+++ src/main/java/com/example/bitcoinpriceapi/BitcoinpriceapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitcoinpriceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinpriceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitcoinpriceapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bitcoinpriceapi/BitcoinpriceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bitcoinpriceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinpriceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
