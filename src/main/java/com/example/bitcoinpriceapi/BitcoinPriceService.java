ff80b1384df6a9730f4019edface07205faccb65
Leandro Damasio <ldamasio@gmail.com>
2024-05-19 02:57:17 -0300

    feat: add functionality to parse Bitcoin price in USD from CoinDesk API response

--- src/main/java/com/example/bitcoinpriceapi/BitcoinPriceService.java
+++ src/main/java/com/example/bitcoinpriceapi/BitcoinPriceService.java
@@ -3,6 +3,9 @@ package com.example.bitcoinpriceapi;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 
 @Service
 public class BitcoinPriceService {
@@ -10,6 +13,9 @@ public class BitcoinPriceService {
 
     public String getBitcoinPrice() {
         String url = "https://api.coindesk.com/v1/bpi/currentprice/BTC.json";
-        return restTemplate.getForObject(url, String.class);
+        String response = restTemplate.getForObject(url, String.class);
+        JsonObject jsonObject = new Gson().fromJson(response, JsonObject.class);
+        String priceInUSD = jsonObject.getAsJsonObject("bpi").getAsJsonObject("USD").get("rate").getAsString();
+        return priceInUSD; 
     }
 }
